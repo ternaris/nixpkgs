ef9e40e0cd3c1fd6b0515b4e821d7fd05106ba25
Florian Friesdorf <flo@chaoflow.net>
2014-10-23 16:02:52 +0200

    cygwin: stdenvNative

--- pkgs/stdenv/native/default.nix
+++ pkgs/stdenv/native/default.nix
@@ -12,65 +12,80 @@ rec {
     (if system == "x86_64-solaris" then [ "/opt/local/gnu" ] else []) ++
     ["/" "/usr" "/usr/local"];
 
-  prehookBase = ''
+
+  preHookBase = ''
     # Disable purity tests; it's allowed (even needed) to link to
     # libraries outside the Nix store (like the C library).
     export NIX_ENFORCE_PURITY=
   '';
 
-  prehookDarwin = ''
-    ${prehookBase}
+  preHookDarwin = ''
+    ${preHookBase}
+    alias sed-i='sed -i ""'
     export NIX_DONT_SET_RPATH=1
     export NIX_NO_SELF_RPATH=1
     ${import ../darwin/prehook.nix}
   '';
 
-  prehookFreeBSD = ''
-    ${prehookBase}
+  # XXX: We need to handle Darwin with bsdtools anyway. Is it sane to
+  # require and alias gnu tools on *BSD below?
+
+  preHookFreeBSD = ''
+    ${preHookBase}
 
     alias make=gmake
     alias tar=gtar
     alias sed=gsed
+    alias sed-i='gsed -i""'
     export MAKE=gmake
     shopt -s expand_aliases
   '';
 
-  prehookOpenBSD = ''
-    ${prehookBase}
+  preHookOpenBSD = ''
+    ${preHookBase}
 
     alias make=gmake
     alias grep=ggrep
     alias mv=gmv
     alias ln=gln
     alias sed=gsed
+    alias sed-i='gsed -i""'
     alias tar=gtar
 
     export MAKE=gmake
     shopt -s expand_aliases
   '';
 
-  prehookNetBSD = ''
-    ${prehookBase}
+  preHookNetBSD = ''
+    ${preHookBase}
 
     alias make=gmake
     alias sed=gsed
+    alias sed-i='gsed -i""'
     alias tar=gtar
     export MAKE=gmake
     shopt -s expand_aliases
   '';
 
-  # CYGWINTODO
+  # Only used to build stdenvCygwin, check cygwin/default.nix.
+  preHookCygwin = ''
+    ${preHookBase}
 
-  prehookCygwin = ''
-    ${prehookBase}
+    alias sed-i='sed -i""'
+    shopt -s expand_aliases
 
-    if test -z "$cygwinConfigureEnableShared"; then
-      export configureFlags="$configureFlags --disable-shared"
-    fi
-
-    PATH_DELIMITER=';'
+    # prevent libtool from failing to find dynamic libraries
+    export lt_cv_deplibs_check_method=pass_all
   '';
 
+  extraBuildInputsCygwin = [
+    ../cygwin/all-buildinputs-as-runtimedep.sh
+    ../cygwin/wrap-exes-to-find-dlls.sh
+  ] ++ (if system == "i686-cygwin" then [
+    ../cygwin/rebase-i686.sh
+  ] else if system == "x86_64-cygwin" then [
+    ../cygwin/rebase-x86_64.sh
+  ] else []);
 
   # A function that builds a "native" stdenv (one that uses tools in
   # /usr etc.).
@@ -79,12 +94,19 @@ rec {
 
     import ../generic {
       preHook =
-        if system == "x86_64-darwin" then prehookDarwin else
-        if system == "i686-freebsd" then prehookFreeBSD else
-        if system == "x86_64-freebsd" then prehookFreeBSD else
-        if system == "i686-openbsd" then prehookOpenBSD else
-        if system == "i686-netbsd" then prehookNetBSD else
-        prehookBase;
+        if system == "x86_64-darwin" then preHookDarwin else
+        if system == "i686-freebsd" then preHookFreeBSD else
+        if system == "x86_64-freebsd" then preHookFreeBSD else
+        if system == "i686-openbsd" then preHookOpenBSD else
+        if system == "i686-netbsd" then preHookNetBSD else
+        if system == "i686-cygwin" then preHookCygwin else
+        if system == "x86_64-cygwin" then preHookCygwin else
+        preHookBase;
+
+      extraBuildInputs =
+        if system == "i686-cygwin" then extraBuildInputsCygwin else
+        if system == "x86_64-cygwin" then extraBuildInputsCygwin else
+        [];
 
       initialPath = extraPath ++ path;
 
