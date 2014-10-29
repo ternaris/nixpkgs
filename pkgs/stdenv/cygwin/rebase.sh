9b8397523b7cd5168140f7a1137851323eecdadc
Florian Friesdorf <flo@chaoflow.net>
2014-10-29 21:13:31 +0100

    new rebase approach

--- pkgs/stdenv/cygwin/rebase.sh
+++ pkgs/stdenv/cygwin/rebase.sh
@@ -3,14 +3,20 @@ postFixupHooks+=(_cygwinFixAutoImageBase)
 _cygwinFixAutoImageBase() {
     if [ $(uname -m) != "x86_64" ]; then
         find $out -name "*.dll" | while read DLL; do
+            if [ -f /etc/rebasenix.nextbase ]; then
+                NEXTBASE="$(</etc/rebasenix.nextbase)"
+            fi
+            NEXTBASE=${NEXTBASE:-0x61500000}
+
             REBASE=(`/bin/rebase -i $DLL`)
             BASE=${REBASE[2]}
+            SIZE=${REBASE[4]}
 
-            if [ $(($BASE)) -gt $((0x70000000)) ]; then
-                NEWBASE="0x`printf %x $(($BASE-0x20000000))`"
-                echo "REBASE FIX: $DLL $BASE -> $NEWBASE"
-                /bin/rebase -b $NEWBASE $DLL
-            fi
+            echo "REBASE FIX: $DLL $BASE -> $NEXTBASE"
+            /bin/rebase -b $NEXTBASE $DLL
+            NEXTBASE="0x`printf %x $(($NEXTBASE+$SIZE))`"
+
+            echo $NEXTBASE > /etc/rebasenix.nextbase
         done
     fi
 }
