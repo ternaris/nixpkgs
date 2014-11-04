792f00c76d9de98a9b8d068ff2690d5e7c0fca93
Florian Friesdorf <flo@chaoflow.net>
2014-11-04 18:58:50 +0100

    fix wrapper

--- pkgs/stdenv/cygwin/wrap-exes-to-find-dlls.sh
+++ pkgs/stdenv/cygwin/wrap-exes-to-find-dlls.sh
@@ -4,18 +4,18 @@ _cygwinWrapExesToFindDlls() {
     find $out -name "*.exe" | while read EXE; do
         mv "${EXE}" "${EXE}.tmp"
 
-        DLLPATH="${out}/bin"
+        DLLPATH=
         for x in $buildInputs $nativeBuildInputs; do
             if [ -d "${x}/bin" ]; then
-                DLLPATH="${DLLPATH}:${x}/bin"
+                DLLPATH="${DLLPATH}:${DLLPATH:+:}${x}/bin"
             fi
         done
 
         WRAPPER="${EXE%.exe}"
         cat >"${WRAPPER}" <<EOF
 #!/bin/sh
-export PATH=$DLLPATH\${PATH:+:}\${PATH}
-exec "\${0}.exe"
+export PATH=$DLLPATH${DLLPATH:+:}\${PATH}
+exec "\$0.exe" "\$@"
 EOF
         chmod +x "${WRAPPER}"
         mv "${EXE}.tmp" "${EXE}"
